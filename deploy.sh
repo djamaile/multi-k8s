46901d0db0f46560db927fb71fa8a5811af5eeed
djamaile <djamaile.rahamat@minbzk.nl>
2019-12-24 21:56:54 +0100

    deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t djam97/multi-client:latest -t djam97/multie-client:$SHA -f ./client/Dockerfile ./client
+docker build -t djam97/multi-server:latest djam97/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t djam97/multi-worker:latest djam97/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push djam97/multi-client:latest
+docker push djam97/multi-server:latest
+docker push djam97/multi-worker:latest
+
+docker push djam97/multi-client:$SHA
+docker push djam97/multi-server:$SHA
+docker push djam97/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=djam97/multi-server:$SHA
+kubectl set image deployments/client-deployment client=djam97/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=djam97/multi-worker:$SHA
